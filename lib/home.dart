4592ebae71823a7c582d738ffb5b667f65840eee
sigu-qucnd <evt.sgtmt@gmail.com>
2020-07-28 15:34:25 +0900

    add route #4

---
+++ lib/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Welcome to Flutter'),
+          ),
+          body:Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 32,bottom:20),
+                child: Text('消費税計算アプリ'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text("開始"),
+                    color: Colors.white,
+                    shape: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    onPressed: () {},
+                  ),
+                  RaisedButton(
+                    child: Text("説明"),
+                    color: Colors.white,
+                    shape: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
