f4eb5ac518181d6507bb16f52a61247a3c2cdc8c
sigu-qucnd <evt.sgtmt@gmail.com>
2020-07-30 20:15:16 +0900

    add create pages #2

--- lib/consumption.dart
+++ lib/consumption.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
+
 class Consumption extends StatelessWidget {
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
@@ -13,11 +12,52 @@ class Consumption extends StatelessWidget {
             children: <Widget>[
               Container(
                 padding: EdgeInsets.only(top: 32,bottom:20),
-                child: Text('計算画面'),
+                child: Text('入力画面'),
               ),
+              Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        const Text(
+                          '金額 : ',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Flexible(
+                          child:TextField(keyboardType: TextInputType.number,)
+                        )
+                      ]
+                    )
+                  ]
+              ),
+              Column(
+                  children: <Widget>[
+                    Row(
+                        children: <Widget>[
+                          const Text(
+                            '税率 : ',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          Flexible(
+                              child:TextField(keyboardType: TextInputType.number,)
+                          )
+                        ]
+                    )
+                  ]
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 12,bottom:20),
+                child: RaisedButton(
+                  child: Text("計算"),
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  onPressed: () {},
+                ),
+              )
+
             ],
           )
-      ),
     );
   }
 }
--- lib/messageInfo.dart
+++ lib/messageInfo.dart
@@ -4,7 +4,6 @@ class MessageInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-
           appBar: AppBar(
             title: const Text('Welcome to Flutter'),
           ),
